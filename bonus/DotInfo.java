1d862b4895e76c14e1db14cca0ad5a18318b2a28
EricHaggar <erichaggar@hotmail.com>
2019-01-04 01:15:56 -0500

    Cleaned-up classes

--- bonus/DotInfo.java
+++ bonus/DotInfo.java
@@ -1,19 +1,9 @@
-//Student name: Eric Haggar
-//Student number: 7674509
-//Course code: ITI1521
-//Lab section: A-2
-
 /**
-* The class <b>DotInfo</b> is a simple helper class to store
-* the state (e.g. clicked, mined, number of neighbooring mines...)
-* at the dot position (x,y)
-*
-* @author Guy-Vincent Jourdan, University of Ottawa
-*/
+ * The class <b>DotInfo</b> is a simple helper class to store the state (e.g.
+ * clicked, mined, number of neighbooring mines...) at the dot position (x,y)
+ */
 
 public class DotInfo {
-
-  // ADD YOUR INSTANCE VARIABLES HERE
 
   private boolean covered;
   private boolean mined;
@@ -24,16 +14,13 @@ public class DotInfo {
   private boolean flagged;
 
   /**
-  * Constructor, used to initialize the instance variables
-  *
-  * @param x
-  *            the x coordinate
-  * @param y
-  *            the y coordinate
-  */
-  public DotInfo(int x, int y){
+   * Constructor, used to initialize the instance variables
+   *
+   * @param x the x coordinate
+   * @param y the y coordinate
+   */
+  public DotInfo(int x, int y) {
 
-    // ADD YOU CODE HERE
     this.x = x;
     this.y = y;
     covered = true;
@@ -45,148 +32,101 @@ public class DotInfo {
   }
 
   /**
-  * Getter method for the attribute x.
-  *
-  * @return the value of the attribute x
-  */
-  public int getX(){
-
-    // ADD YOU CODE HERE
-
+   * Getter method for the attribute x.
+   *
+   * @return the value of the attribute x
+   */
+  public int getX() {
     return this.x;
-
   }
 
   /**
-  * Getter method for the attribute y.
-  *
-  * @return the value of the attribute y
-  */
-  public int getY(){
-
-    // ADD YOU CODE HERE
-
+   * Getter method for the attribute y.
+   *
+   * @return the value of the attribute y
+   */
+  public int getY() {
     return this.y;
-
   }
 
-
   /**
-  * Setter for mined
-  */
+   * Setter for mined
+   */
   public void setMined() {
-
-    // ADD YOU CODE HERE
-
     mined = true;
-
   }
 
   /**
-  * Getter for mined
-  *
-  * @return mined
-  */
+   * Getter for mined
+   *
+   * @return mined
+   */
   public boolean isMined() {
-
-    // ADD YOU CODE HERE
-
     return mined;
-
   }
 
-
   /**
-  * Setter for covered
-  */
+   * Setter for covered
+   */
   public void uncover() {
-
-    // ADD YOU CODE HERE
-
     covered = false;
-
   }
 
   /**
-  * Getter for covered
-  *
-  * @return covered
-  */
-  public boolean isCovered(){
-
-    // ADD YOU CODE HERE
+   * Getter for covered
+   *
+   * @return covered
+   */
+  public boolean isCovered() {
     return covered;
-
   }
 
-
   /**
-  * Setter for wasClicked
-  */
+   * Setter for wasClicked
+   */
   public void click() {
-
-    // ADD YOU CODE HERE
-
     wasClicked = true;
-
   }
 
-
   /**
-  * Getter for wasClicked
-  *
-  * @return wasClicked
-  */
+   * Getter for wasClicked
+   *
+   * @return wasClicked
+   */
   public boolean hasBeenClicked() {
-
-    // ADD YOU CODE HERE
-
     return wasClicked;
-
   }
 
-
   /**
-  * Setter for neighbooringMines
-  *
-  * @param neighbooringMines
-  *          number of neighbooring mines
-  */
+   * Setter for neighbooringMines
+   *
+   * @param neighbooringMines number of neighbooring mines
+   */
   public void setNeighbooringMines(int neighbooringMines) {
-
-    // ADD YOU CODE HERE
-
     this.neighbooringMines = neighbooringMines;
-
   }
 
   /**
-  * Get for neighbooringMines
-  *
-  * @return neighbooringMines
-  */
+   * Get for neighbooringMines
+   *
+   * @return neighbooringMines
+   */
   public int getNeighbooringMines() {
-
-    // ADD YOU CODE HERE
-
     return neighbooringMines;
-
   }
 
-
   /**
-  * Setter for flagged to set it to true
-  */
+   * Setter for flagged to set it to true
+   */
   public void flag() {
 
     flagged = true;
 
-
   }
 
   /**
-  * Setter for flagged to set it to false
-  */
+   * Setter for flagged to set it to false
+   */
   public void unflag() {
 
     flagged = false;
@@ -194,15 +134,14 @@ public class DotInfo {
   }
 
   /**
-  * Getter for flagged
-  *
-  * @return flagged
-  */
-  public boolean isFlagged(){
+   * Getter for flagged
+   *
+   * @return flagged
+   */
+  public boolean isFlagged() {
 
     return flagged;
 
   }
-
 
 }
--- bonus/GameView.java
+++ bonus/GameView.java
@@ -1,25 +1,16 @@
-//Student name: Eric Haggar
-//Student number: 7674509
-//Course code: ITI1521
-//Lab section: A-2
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 /**
-* The class <b>GameView</b> provides the current view of the entire Game. It extends
-* <b>JFrame</b> and lays out a matrix of <b>DotButton</b> (the actual game) and
-* two instances of JButton. The action listener for the buttons is the controller.
-*
-* @author Guy-Vincent Jourdan, University of Ottawa
-*/
+ * The class <b>GameView</b> provides the current view of the entire Game. It
+ * extends <b>JFrame</b> and lays out a matrix of <b>DotButton</b> (the actual
+ * game) and two instances of JButton. The action listener for the buttons is
+ * the controller.
+ */
 
 public class GameView extends JFrame {
-
-  // ADD YOUR INSTANCE VARIABLES HERE
 
   private DotButton[][] board;
   private GameModel gameModel;
@@ -27,47 +18,44 @@ public class GameView extends JFrame {
   private JLabel differenceLabel;
 
   /**
-  * Constructor used for initializing the Frame
-  *
-  * @param gameModel
-  *            the model of the game (already initialized)
-  * @param gameController
-  *            the controller
-  */
+   * Constructor used for initializing the Frame
+   *
+   * @param gameModel      the model of the game (already initialized)
+   * @param gameController the controller
+   */
 
   public GameView(GameModel gameModel, GameController gameController) {
 
-    // ADD YOU CODE HERE
+    // sets the title
+    super("MineSweeper");
 
-    //sets the title
-    super("MineSweeper - The ITI 1521 Version");
-
-    //initializing variables to be used
+    // initializing variables to be used
     this.gameModel = gameModel;
     board = new DotButton[gameModel.getHeigth()][gameModel.getWidth()];
     nbreOfStepsLabel = new JLabel("Number of steps: " + gameModel.getNumberOfSteps() + ", ");
     int difference = gameModel.getNumberOfMines() - gameModel.getNumberOfFlags();
     differenceLabel = new JLabel("Difference between Mines and Flag: " + difference);
 
-    //Frame
+    // Frame
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBackground(Color.WHITE);
 
-    //create JPanel for grid (DotButtons)
+    // create JPanel for grid (DotButtons)
     JPanel grid = new JPanel();
     grid.setLayout(new GridLayout(gameModel.getHeigth(), gameModel.getWidth()));
     grid.setBackground(Color.WHITE);
 
-    //Creat JPanel for bottom panel (number of steps, reset, quit)
+    // Creat JPanel for bottom panel (number of steps, reset, quit)
     JPanel bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
     bottom.setBackground(Color.WHITE);
     bottom.add(nbreOfStepsLabel);
     bottom.add(differenceLabel);
 
-    //Populate the board with DotButton instances and addActionListener for each button
-    for(int i = 0; i < gameModel.getHeigth(); i++){
-      for(int j = 0; j < gameModel.getWidth(); j++){
-        board[i][j] = new DotButton(j,i,11);
+    // Populate the board with DotButton instances and addActionListener for each
+    // button
+    for (int i = 0; i < gameModel.getHeigth(); i++) {
+      for (int j = 0; j < gameModel.getWidth(); j++) {
+        board[i][j] = new DotButton(j, i, 11);
         board[i][j].addMouseListener(gameController);
         grid.add(board[i][j]);
 
@@ -76,7 +64,7 @@ public class GameView extends JFrame {
 
     add(grid, BorderLayout.CENTER);
 
-    //Create JButtons for reset and quit and addActionListener for each button
+    // Create JButtons for reset and quit and addActionListener for each button
     JButton reset = new JButton("Reset");
     reset.setBackground(Color.WHITE);
     reset.addMouseListener(gameController);
@@ -86,7 +74,7 @@ public class GameView extends JFrame {
     quit.setBackground(Color.WHITE);
     quit.addMouseListener(gameController);
     bottom.add(quit);
-    add(bottom,BorderLayout.SOUTH);
+    add(bottom, BorderLayout.SOUTH);
 
     setResizable(false);
     setLocationRelativeTo(null);
@@ -96,20 +84,17 @@ public class GameView extends JFrame {
   }
 
   /**
-  * update the status of the board's DotButton instances based
-  * on the current game model, then redraws the view
-  */
+   * update the status of the board's DotButton instances based on the current
+   * game model, then redraws the view
+   */
 
-  public void update(){
-
-    // ADD YOU CODE HERE
-
+  public void update() {
 
     for (int i = 0; i < gameModel.getHeigth(); i++) {
 
       for (int j = 0; j < gameModel.getWidth(); j++) {
 
-        board[i][j].setIconNumber(getIcon(i,j));
+        board[i][j].setIconNumber(getIcon(i, j));
         nbreOfStepsLabel.setText("Number of steps: " + gameModel.getNumberOfSteps() + ", ");
         int difference = gameModel.getNumberOfMines() - gameModel.getNumberOfFlags();
         differenceLabel.setText("Difference between Mines and Flag: " + difference);
@@ -117,53 +102,44 @@ public class GameView extends JFrame {
       }
     }
 
-
-
   }
 
   /**
-  * returns the icon value that must be used for a given dot
-  * in the game
-  *
-  * @param i
-  *            the x coordinate of the dot
-  * @param j
-  *            the y coordinate of the dot
-  * @return the icon to use for the dot at location (i,j)
-  */
-  private int getIcon(int i, int j){
-
-    // ADD YOU CODE HERE
+   * returns the icon value that must be used for a given dot in the game
+   *
+   * @param i the x coordinate of the dot
+   * @param j the y coordinate of the dot
+   * @return the icon to use for the dot at location (i,j)
+   */
+  private int getIcon(int i, int j) {
 
     int number;
 
-    if (gameModel.isCovered(i,j) && !(gameModel.isFlagged(i,j))) {
+    if (gameModel.isCovered(i, j) && !(gameModel.isFlagged(i, j))) {
 
       number = 11;
 
-    } else if (gameModel.isMined(i,j) && gameModel.hasBeenClicked(i,j)) {
+    } else if (gameModel.isMined(i, j) && gameModel.hasBeenClicked(i, j)) {
 
       number = 10;
 
-    } else if (gameModel.isMined(i,j) && !(gameModel.isFlagged(i,j))) {
+    } else if (gameModel.isMined(i, j) && !(gameModel.isFlagged(i, j))) {
 
       number = 9;
 
-    } else if (gameModel.isCovered(i,j) && gameModel.isFlagged(i,j)) {
+    } else if (gameModel.isCovered(i, j) && gameModel.isFlagged(i, j)) {
 
       number = 12;
     }
 
     else {
 
-      number = gameModel.getNeighbooringMines(i,j);
+      number = gameModel.getNeighbooringMines(i, j);
 
     }
-
 
     return number;
 
   }
-
 
 }
